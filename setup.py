5e449f3efb9bea086aa985794b26120e52133656
MemoryFr <71544305+Ivan-Kulagin@users.noreply.github.com>
2021-04-02 23:27:07 +0300

    upd

--- setup.py
+++ setup.py
@@ -1,30 +1,19 @@
 from setuptools import setup
-from sphinx.setup_command import BuildDoc
 
-
-cmdclass = {'build_sphinx': BuildDoc}
-name = 'weather',
-version='0.0.1',
-release='0.0.1'
 
 setup(
-    name=name,
-    author=['Bartenev', 'Kulagin'],
-    author_email='b.bar1enev@gmail.com',
-    url='https://github.com/Flamer322/bot',
-    version=release,
-    cmdclass=cmdclass,
-    packages=['weather'],
-    install_requires=[
-        'requests',
-        'pytelegrambotapi',
-        'sphinx',
-        'importlib; python_version == "3.9"',
-    ],
-    #command_options={
-    #    'build_sphinx': {
-    #        'project': ('setup.py', name),
-    #        'version': ('setup.py', version),
-    #        'release': ('setup.py', release),
-    #        'source_dir': ('setup.py', 'doc')}},
+  name='WeatherBotKB',
+  version='0.0.3',
+  username='Ivan-Kulagin',
+  author='Kulagin & Bartenev',
+  author_email='b.bar1enev@gmail.com',
+  packages=['weather'],
+  url='https://github.com/Flamer322/bot',
+  description='Telegram API based WeatherBot',
+  long_description=open('README.md').read(),
+  long_description_content_type="text/markdown",
+  install_requires=[
+      "requests",
+      "pyTelegramBotAPI"
+  ],
 )
