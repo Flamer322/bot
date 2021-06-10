44a9d48bdd62debcdc842a47764c47c03d3b3350
Flamer322 <56824837+Flamer322@users.noreply.github.com>
2021-06-10 14:53:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(
-  name='WeatherBotKB',
-  version='0.0.3',
-  username='Ivan-Kulagin',
-  author='Kulagin & Bartenev',
-  author_email='b.bar1enev@gmail.com; ivan.kulagin2001@gmail.com',
+  name='WeatherBotB',
+  version='0.0.51',
+  username='flamer322',
+  author='flamer322',
+  author_email='flamer322@yandex.ru',
   packages=['weather'],
   url='https://github.com/Flamer322/bot',
   description='Telegram API based WeatherBot',
